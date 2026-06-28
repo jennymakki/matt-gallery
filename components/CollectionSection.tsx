31e2b25ef5d5d9f7167faf84d4943d7e4ea34da9
jennymakki <jjennyeklundd@hotmail.com>
2026-06-28 22:14:38 +0200

    changing order of text and image on mobile

--- components/CollectionSection.tsx
+++ components/CollectionSection.tsx
@@ -16,29 +16,22 @@ export function CollectionSection({
   return (
     <Reveal>
       <section className="py-1 bg-white">
-        <div className="max-w-7xl mx-auto px-6 lg:px-10">
+        <div className="max-w-7xl py-20 mx-auto px-6 lg:px-10">
           <div
             className={`
-              grid lg:grid-cols-2 gap-17 items-center
-              ${reverse ? "lg:flex-row-reverse" : ""}
+              grid lg:grid-cols-2 gap-15 items-center
             `}
           >
-            <div className={reverse ? "lg:order-2" : ""}>
-              <div className="aspect-[4/5] overflow-hidden">
-                <img
-                  src={image}
-                  alt={title}
-                  className="w-full h-full object-contain transition-transform duration-1000 hover:scale-105"
-                />
-              </div>
-            </div>
-
-            <div className={reverse ? "lg:order-1" : ""}>
+            <div
+              className={
+                reverse ? "lg:order-1" : "lg:order-2"
+              }
+            >
               <div
                 className={`
-      max-w-md
-      ${reverse ? "lg:ml-auto lg:text-right" : ""}
-    `}
+                  max-w-md
+                  ${reverse ? "lg:ml-auto lg:text-right" : ""}
+                `}
               >
                 <p className="uppercase tracking-[0.3em] text-xs text-neutral-400 mb-4">
                   Collection
@@ -50,11 +43,23 @@ export function CollectionSection({
                   {description}
                 </p>
 
-                <button
-                  className="border border-black px-8 py-4 hover:bg-black cursor-pointer hover:text-white transition"
-                >
+                <button className="border border-black px-8 py-4 hover:bg-black cursor-pointer hover:text-white transition">
                   View collection →
                 </button>
+              </div>
+            </div>
+
+            <div
+              className={
+                reverse ? "lg:order-2" : "lg:order-1"
+              }
+            >
+              <div className="aspect-[4/5] overflow-hidden">
+                <img
+                  src={image}
+                  alt={title}
+                  className="w-full h-full object-contain transition-transform duration-1000 hover:scale-105"
+                />
               </div>
             </div>
           </div>
